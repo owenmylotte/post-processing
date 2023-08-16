3682b0930dfc746dd293a7d89937446743115e5c
owenmylotte <owenmylotte@gmail.com>
2023-08-15 20:28:17 -0400

    Start plotting flux contours and comparison.

--- slabFluxPlots.py
+++ slabFluxPlots.py
@@ -5,14 +5,26 @@ import matplotlib.ticker as ticker
 import tikzplotlib
 from chrest.chrestData import ChrestData
 
-# Take experimental data and plot contour.
+# Define a variable to hold the flux averages.
+flux_averages = np.zeros(3)  # This will be indexed based on the following definitions
+experimental_index = 0
+original_compute_index = 1
+dns_index = 2
+magic_index = 10  # TODO: Update this with the index of the slice which the boundary sits on.
 
-data_flux = ChrestData(input['base_path'] + "/" + input['dns'])
+# %% Take experimental data and plot contour.
+# TODO: Read in the experimental data from the matlab file in whatever encoding it's currently saved in.
+# Compute the average of the experimental data computed from the tcp
+flux_averages[experimental_index] = 0  # TODO: Take the average of the experimental data.
 
-# Take the heat flux in from ablate data.
-# Plot heat flux contour on top plot.
-# Calculate average heat flux.
+# %% Take the heat flux in from ablate data.
+# Define the slab flux data name as whatever is saved in the data file.
+slab_flux_name = "Slab Flux Name"  # TODO: Update this with the actual slab flux name.
 
+data_boundary = ChrestData(input['base_path'] + "/" + input['dns'])
+flux = data_boundary.get_field(slab_flux_name)
+
+# THIS CAN BE USED AS REFERENCE FOR IMPLEMENTING THE CHREST FORMAT FUNCTIONS.
 # def get_optical_thickness(self, dns_data):
 #     # Calculate the optical thickness of the frame
 #     # First get the absorption for each cell in the dns
@@ -28,11 +40,24 @@ data_flux = ChrestData(input['base_path'] + "/" + input['dns'])
 #         dns_sum_soot = kappa.sum(axis=axis, keepdims=True)
 #         setattr(self, attribute, dns_sum_soot * dns_data.delta[2 - axis])
 
-# Take the slice of the flux that is on the top face. Maybe just manually find it.
+# Check that the slab flux exists, get the field out of the object.
+if flux is None:
+    raise Exception(slab_flux_name + " does not exist.")
 
+# Take the average of the slab flux where the value is not zero.
+# (Take the slice of the flux that is on the top face. Maybe just manually find it.)
+flux_averages[dns_index] = np.average(flux[np.where(flux != 0)])  # Is this the correct way to do this?
 
+# %% Take previous model data and plot contour.
+# TODO: Read in the experimental data from the MATLAB file in whatever encoding it's currently saved in.
+# Compute the average of the experimental data computed from the tcp
+flux_averages[original_compute_index] = 0  # TODO: Average the original model flux and compare it to the current model.
 
-# Take previous model data and plot contour.
+# %% Plot heat flux contour on top plot.
+# TODO: Mesh grid stuff with the slice of the contour on the magic index.
+plt.plot(flux[:, :, magic_index])  # TODO: Update this to plot a proper contour.
 
-
-# Compute the error on the heat flux between the experimental, previous model, and current model.
+# %% Compute the error on the heat flux between the experimental, previous model, and current model.
+# TODO: Error on between each of the entries in the fluxes.
+error = np.zeros(len(flux_averages - 1))
+print(error)
